68c7def06acda77f3f290f35685139d7ad6d8b33
Gezerj <JonesG71@cardiff.ac.uk>
2016-10-17 19:00:48 +0100

    Runga kutta

--- Runge-Kutta Earth Sun.py
+++ Runge-Kutta Earth Sun.py
@@ -10,34 +10,34 @@ import matplotlib.pyplot as plt
 
 from ODE import ODEsolve, ConvergenceTest
 
-def f(t, y, l):
+def f(t, r, l):
     """ A function to call yprime and y """
-    return y[1], l*y[0]
+    return r[1], (l)*r[0],
     
-def Rk4(ya, f, t, dt, l):
+def Rk4(ra, f, t, dt, l):
     """ The Y array has two values so we need to split them up 
     when using euler"""
 #    f1,f2 = f(t, y, l)   # f1 gives the value y of the function and f2 gives the value of yprime of the function
-    yi,ypi = ya        # yb gives the initial y values and yc gives initail yp values
+    ri,rpi = ra        # yb gives the initial y values and yc gives initail yp values
 
     # using Runge-Kutta method
-    k1 = f(t, ya, l)
+    k1 = f(t, ra, l)
     ki1 = np.array(k1)
-    k2 = f(t,ya + ki1*dt/2, l)
+    k2 = f(t,ra + ki1*dt/2, l)
     ki2 = np.array(k2)
-    k3 = f(t,ya + ki2*dt/2, l)
+    k3 = f(t,ra + ki2*dt/2, l)
     ki3 = np.array(k3)
-    k4 = f(t,ya + ki3*dt, l)  
+    k4 = f(t,ra + ki3*dt, l)  
   
-    y = yi + (dt/6)*(k1[0] + 2*k2[0] + 2*k3[0] + k4[0])  # y value
-    yp = ypi + (dt/6)*(k1[1] + 2*k2[1] + 2*k3[1] + k4[1])  # yprime value
+    r = ri + (dt/6)*(k1[0] + 2*k2[0] + 2*k3[0] + k4[0])  # y value
+    rp = rpi + (dt/6)*(k1[1] + 2*k2[1] + 2*k3[1] + k4[1])  # yprime value
 #    
-    return y, yp
+    return r, rp
 
 #lambda
 G = 6.67408e-11; Me = 5.9724e24; Ms = 1.9885e30
 R = 1.496e11
-l = (G*Ms*Me)/(R**3)
+l = -(G*Ms*Me)/(R**3)
 
 #defining initial conditions 
 ti = 0; Tmax = 3.154e7
@@ -50,11 +50,11 @@ N = 1e8
 ic = np.array([ti, R, v, l])  # initial time, final time, initial y and lambda
 
 # solving ODE
-R = ODEsolve(Tmax, N, f, Rk4, ic) 
+X = ODEsolve(Tmax, N, f, Rk4, ic) 
 
-T = R[0][1]; Y = R[0][0]
+T = X[0][1]; Y = X[0][0]
 
-plt.plot(T, Y, label=r'$dt = %.5f$' %(Tmax/n[i]))
+plt.plot(T, Y, label=r'$dt = %.5f$' %(Tmax/N))
 plt.xlabel(r'$Time$')
 plt.ylabel(r'$Y$') 
 plt.title('Graph of ODE')
